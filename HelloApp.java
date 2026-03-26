d78057d219d1ce12a784e9af68d3a092015aa243
RitvikBalaji <ritvikbk@gmail.com>
2026-03-26 10:55:35 +0530

    Added UC7: Implemented String.join method to format multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,8 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
-            String names = "";
-            // Use enhanced for loop to build the string
-            for (String name : args) {
-                names += name + ", ";
-            }
-            
-            // Concept: substring(startIndex, endIndex) 
-            // We remove the last 2 characters (the extra comma and space)
-            names = names.substring(0, names.length() - 2);
-            
+            // Concept: String.join(delimiter, elements)
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         } else {
             System.out.println("Hello, World!");
