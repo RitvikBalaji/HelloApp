e3dc57351e2b27155c91f0bbf09a00eb64bd3762
RitvikBalaji <ritvikbk@gmail.com>
2026-03-26 10:31:30 +0530

    feat: implement UC3 display name with default fallback

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name= args[0];
-        System.out.println("Hello!" + name + "!");
+        // Check if a command-line argument is provided
+        if (args.length > 0) {
+            // Personalize the greeting with the first argument
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            // Default greeting if no argument is passed
+            System.out.println("Hello, World!");
+        }
     }
 }
